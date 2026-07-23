7a88d195301fd984e84e74a08b89160fd0b5d112
Stephen <159204715+StephenK0@users.noreply.github.com>
2026-07-23 13:13:05 -0400

    Add method headers for public methods in Game.cs and rearrange the object hierarchy to be consistent.

---
+++ Assets/Scripts/Systems/Game.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+/*
+ * Responsible for the main gameplay loop, such as keeping track of how many rounds the player has completed. 
+ */
+public class Game : MonoBehaviour
+{
+	public void PlayerCompleteSet() {}
+
+	public void PlayerMistake() {}
+}
