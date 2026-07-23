8871ede62f8f178a1e9e52667611763a0fcedbd9
Arturo Kojiki <arturo.kojiki@gmail.com>
2026-07-23 17:01:08 -0400

    Called PlayerCompleteSet() When a Set is Complete and Added a Way to Track the Number of Complete Sets

--- Assets/Scripts/Cards/MainPlayingCard.cs
+++ Assets/Scripts/Cards/MainPlayingCard.cs
@@ -7,12 +7,18 @@ public class MainPlayingCard : Card
     // Remove the card when the player clicks correctly and trigger a mistake otherwise.
     public void OnMouseDown()
     {
-        this.onField = true;
+        this.onField = true;  // TODO: remove?
 
         if (value == CardManager.nextCardToClick)
         {
             CardManager.nextCardToClick -= 1;
             this.Remove();
+
+            if (CardManager.nextCardToClick == 0)
+            {
+                Debug.Log("Set complete!");
+                Game.main.PlayerCompleteSet();
+            }
         }
         else
         {
