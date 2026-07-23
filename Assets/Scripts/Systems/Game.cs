d4b8835d2f3152caa266e06d29ecaf5ce9055cb5
Stephen <159204715+StephenK0@users.noreply.github.com>
2026-07-23 13:38:46 -0400

    Add a publicly accessable static instance of Game to itself.

--- Assets/Scripts/Systems/Game.cs
+++ Assets/Scripts/Systems/Game.cs
@@ -4,6 +4,8 @@ using UnityEngine;
  */
 public class Game : MonoBehaviour
 {
+	public static Game main { get; private set; }
+	
 	public void PlayerCompleteSet() {}
 
 	public void PlayerMistake() {}
