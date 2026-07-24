9b5784ea2a7a51cadb326b26a928de48bb23c602
Arturo Kojiki <arturo.kojiki@gmail.com>
2026-07-24 12:32:38 -0400

    ObstacleCard Movement Implemeneted for the Menu

--- Assets/Scripts/Cards/MainPlayingCard.cs
+++ Assets/Scripts/Cards/MainPlayingCard.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 public class MainPlayingCard : Card
 {
     [SerializeField] int value;
-    [SerializeField] Vector2 goalPosition;
-    private Vector2 startPosition;
 
     // Remove the card when the player clicks correctly and trigger a mistake otherwise.
     public void OnMouseDown()
@@ -25,39 +23,6 @@ public class MainPlayingCard : Card
         else
         {
             Game.main.PlayerMistake();
-        }
-    }
-
-    private void Start()
-    {
-        startPosition.y = transform.position.y;
-    }
-
-    // Move the card on menu screens.
-    void Update()
-    {
-        float step;
-        if (goalPosition.y > 0)
-        {
-            step = 90 * Time.deltaTime;
-        }
-        else
-        {
-            step = -90 * Time.deltaTime;
-        }
-
-        transform.position = new Vector2(transform.position.x, startPosition.y);
-        startPosition.y += step;
-
-        if (transform.position.y > goalPosition.y && goalPosition.y > 0)
-        {
-            transform.position = new Vector2(transform.position.x, -200);
-            startPosition.y = -200;
-        }
-        else if (transform.position.y < goalPosition.y && goalPosition.y < 0)
-        {
-            transform.position = new Vector2(transform.position.x, 600);
-            startPosition.y = 600;
         }
     }
 }
--- Assets/Scripts/Cards/ObstacleCard.cs
+++ Assets/Scripts/Cards/ObstacleCard.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class ObstacleCard : Card
 {
+    [SerializeField] bool isMoving;
+    [SerializeField] Vector2 goalPosition;
+    private Vector2 startPosition;
 
     public void OnMouseDown()
     {
@@ -15,5 +18,41 @@ public class ObstacleCard : Card
     {
         this.onField = true;  // TODO: remove eventually?
         this.Remove();
+    }
+
+    private void Start()
+    {
+        startPosition.y = transform.position.y;
+    }
+
+    // Move the card on menu screens.
+    void Update()
+    {
+        if (isMoving)
+        {
+            float step;
+            if (goalPosition.y > 0)
+            {
+                step = 90 * Time.deltaTime;
+            }
+            else
+            {
+                step = -90 * Time.deltaTime;
+            }
+
+            transform.position = new Vector2(transform.position.x, startPosition.y);
+            startPosition.y += step;
+
+            if (transform.position.y > goalPosition.y && goalPosition.y > 0)
+            {
+                transform.position = new Vector2(transform.position.x, -200);
+                startPosition.y = -200;
+            }
+            else if (transform.position.y < goalPosition.y && goalPosition.y < 0)
+            {
+                transform.position = new Vector2(transform.position.x, 600);
+                startPosition.y = 600;
+            }
+        }
     }
 }
